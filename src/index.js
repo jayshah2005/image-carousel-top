2f3304e73206e3dd0012ace927d38766547a94bb
Jay Shah <jayyshah69@gmail.com>
2024-05-25 21:50:53 -0400

    Created the skeleton of the image carousel.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import './style.css';
+import setup1 from './assests/setup-1.jpg'
+import setup2 from './assests/setup-2.jpg'
 
+const frame = document.querySelector(".picture-frame")
+const imgs = [setup1, setup2]
+
+imgs.forEach((src) => {
+    const imgDiv = document.createElement('div')
+    imgDiv.className = 'imgDiv'
+    const img = document.createElement('img')
+    img.src = src;
+    img.className = 'img'
+    imgDiv.append(img)
+    frame.append(imgDiv)
+})
 
 console.log("It works!")
