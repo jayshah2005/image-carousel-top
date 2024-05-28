2fbc14446bc8c90587067c7dd2a7469c04bc855a
Jay Shah <jayyshah69@gmail.com>
2024-05-28 17:26:14 -0400

    Added a way to move to a particular image using navigation buttons.

--- src/index.js
+++ src/index.js
@@ -1,51 +1,75 @@
-import './style.css';
-import setup1 from './assests/setup-1.jpg'
-import setup2 from './assests/setup-2.jpg'
+import "./style.css";
+import setup1 from "./assests/setup-1.jpg";
+import setup2 from "./assests/setup-2.jpg";
 
-const frame = document.querySelector(".picture-frame")
-const imgs = [setup1, setup2]
-var currImage = document.querySelector(':root');
+const frame = document.querySelector(".picture-frame");
+const imgs = [setup1, setup2];
+var currImage = document.querySelector(":root");
+
+const displacement = 2 * imgs.length + "px";
+const container = document.createElement("div");
+container.className = "container";
+container.style.left = "calc(50% - " + displacement + ")";
+
+frame.append(container);
 
 imgs.forEach((src) => {
-    const imgDiv = document.createElement('div')
-    imgDiv.className = 'imgDiv'
-    imgDiv.style.right = '0px'
-    const img = document.createElement('img')
-    img.src = src;
-    img.className = 'img'
-    imgDiv.append(img)
-    frame.append(imgDiv)
-})
+	const imgDiv = document.createElement("div");
+	imgDiv.className = "imgDiv";
+	imgDiv.style.right = "0px";
+	const img = document.createElement("img");
+	img.src = src;
+	img.className = "img";
+	imgDiv.append(img);
+	frame.append(imgDiv);
 
-const left = document.querySelector(".left")
-const right = document.querySelector(".right")
-const imgDiv = Array.from(document.querySelectorAll('.imgDiv'))
+	const imageKey = document.createElement("div");
+	imageKey.className = "imageKey";
+	container.appendChild(imageKey);
 
+	imageKey.addEventListener('click', (event) => {
+        const imgNumber = imgs.findIndex(elem => elem === src)
+        currImage.style.setProperty("--currImage", imgNumber)
+    });
+});
 
-left.addEventListener('click', (event) => {
-    const root = document.querySelector(":root")
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const imgDiv = Array.from(document.querySelectorAll(".imgDiv"));
 
-    if((getComputedStyle(root).getPropertyValue("--currImage") | 0) > 0){
-        root.style.setProperty("--currImage", (getComputedStyle(root).getPropertyValue("--currImage") | 0) - 1)
-        imgDiv.forEach((div) => {
-            div.style.right = "calc( (100%)*var(--currImage) )";
-        })
-    }
-})
+left.addEventListener("click", (event) => {
+	const root = document.querySelector(":root");
 
-right.addEventListener('click', (event) => {
-    const root = document.querySelector(":root")
+	if ((getComputedStyle(root).getPropertyValue("--currImage") | 0) > 0) {
+		root.style.setProperty(
+			"--currImage",
+			(getComputedStyle(root).getPropertyValue("--currImage") | 0) - 1
+		);
+		imgDiv.forEach((div) => {
+			div.style.right = "calc( (100%)*var(--currImage) )";
+		});
+	}
+});
 
-    if((getComputedStyle(root).getPropertyValue("--currImage") | 0) < imgs.length - 1){
-        root.style.setProperty("--currImage", (getComputedStyle(root).getPropertyValue("--currImage") | 0) + 1)
-        imgDiv.forEach((div) => {
-            div.style.right = "calc( (100%)*var(--currImage) )";
-        })
-    }
-})
+right.addEventListener("click", (event) => {
+	const root = document.querySelector(":root");
+
+	if (
+		(getComputedStyle(root).getPropertyValue("--currImage") | 0) <
+		imgs.length - 1
+	) {
+		root.style.setProperty(
+			"--currImage",
+			(getComputedStyle(root).getPropertyValue("--currImage") | 0) + 1
+		);
+		imgDiv.forEach((div) => {
+			div.style.right = "calc( (100%)*var(--currImage) )";
+		});
+	}
+});
 
 let intervalID = setInterval(() => {
-    const root = document.querySelector(":root")    
+    const root = document.querySelector(":root")
     const currImage = ((getComputedStyle(root).getPropertyValue("--currImage") | 0) + 1) % imgs.length
 
     root.style.setProperty("--currImage", currImage)
@@ -55,5 +79,4 @@ let intervalID = setInterval(() => {
     })
 }, 5000)
 
-
-console.log("It works!")
+console.log("It works!");
